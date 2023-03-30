808795cdadb553ab8b6a6c6937abfc98709ace67
sder12 <sara.cetto@gmail.com>
2023-03-30 20:52:59 +0200

    Pass data with props to Slide component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import Slide from './components/Slide';
+import data from './data';
 
 const App = () => {
   return (
-    <div className="App">
-      SLIDER APP
+    <div className="App section-center">
+      <h2>Our vacation</h2>
+      <Slide />
+
+      {
+        data.map((el) => {
+          return <Slide key={el.id} {...el} />
+        })
+      }
     </div>
   )
 }
---
+++ src/components/Slide.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Slide = (slide) => {
+    return (
+        <div>{slide.autore}</div>
+    )
+}
+
+export default Slide
